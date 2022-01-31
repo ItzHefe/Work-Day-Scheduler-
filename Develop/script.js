5d7af04c9878f42c96734f04a3d3bd9c901fe97a
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-01-31 11:16:40 -0500

    created first variables, outlined the work day hourss

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+var today = moment().format("dddd, MMMM Do");
+var currentTime = moment().format("H A");
+
+/* hourly calendar entries */
+var workDay = [
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
+];
